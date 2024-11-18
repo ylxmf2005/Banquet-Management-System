4527eb941b540506733ce0761da56935b687211d
Sunghak222 <88081894+Sunghak222@users.noreply.github.com>
2024-11-18 14:04:36 +0800

    Update Constructor and Getters to AttendeeAccount.java

--- src/main/model/AttendeeAccount.java
+++ src/main/model/AttendeeAccount.java
@@ -12,4 +12,41 @@ public class AttendeeAccount {
     private String organization; // Affiliated Organization
 
     // Constructors, getters, and setters omitted
+    public AttendeeAccount(String email, String firstName, String lastName, String address, String type,
+                           String password, String mobileNo, String organization) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.type = type;
+        this.password = password;
+        this.mobileNo = mobileNo;
+        this.organization = organization;
+    }
+
+    /*Getters*/
+    public String getEmail() {
+        return email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getType() {
+        return type;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getMobileNo() {
+        return mobileNo;
+    }
+    public String getOrganization() {
+        return organization;
+    }
 }
