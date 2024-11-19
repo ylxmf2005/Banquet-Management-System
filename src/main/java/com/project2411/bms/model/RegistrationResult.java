70220cee35b7e1082a92c3330866e252b781a137
Sunghak222 <88081894+Sunghak222@users.noreply.github.com>
2024-11-19 23:00:15 +0800

    add getters and setters in RegistrationResult.java

--- src/main/java/com/project2411/bms/model/RegistrationResult.java
+++ src/main/java/com/project2411/bms/model/RegistrationResult.java
@@ -12,4 +12,16 @@ public class RegistrationResult {
     }
 
     // Getters and setters omitted
+    public boolean isSuccess() {
+        return success;
+    }
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
